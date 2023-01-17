e9f2d3f141bd063f876b5204dd44ca33442ae2fb
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-01-18 03:07:46 +0530

    create the api and set get route
    Create the server for the API, connect the server to the database, create get request function for /articles route.

---
+++ app.js
@@ -0,0 +1,59 @@
+// importing express
+const express = require("express");
+// importing body-parser
+const bodyParser = require("body-parser");
+// importing mongoose
+const mongoose = require("mongoose");
+// importing ejs
+const ejs = require("ejs");
+
+//Set up default mongoose connection
+var mongoDB = "mongodb://127.0.0.1/wikiDB";
+mongoose.set('strictQuery', false);
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+
+// Creating app using express
+const app = express();
+// Setting the view engine to ejs
+app.set('view engine', 'ejs');
+// Setting the public folder to static
+app.use(express.static("public"));
+
+
+// Creating port
+const port = 3000;
+
+
+// using the bodyParser in urlEncoder mode
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+// get function for "/" route
+app.get("/", (req, res) => {
+  res.send("Wiki-Api");
+});
+
+// get function for "/articles" route
+app.get("/articles", (req, res) => {
+    Article.find({}, {_id:0, title:1, content:1}, (err, articles) => {
+      if (!err){
+        res.send(articles);
+      } else {
+        res.send(err);
+      }
+    });
+});
+
+
+// Listening to the port
+app.listen(port, () => {
+  // Console logging the server starting message
+  console.log("Server is online at port : " + port);
+});
